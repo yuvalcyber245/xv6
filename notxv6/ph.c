5f1850ff4ba865239124f447cdf3708fb21bee7e
yuvalcyber245 <yuval.gome.il@gmail.com>
2024-03-12 02:26:56 +0200

    git commit a second crime (an array of keys, one per bucket)

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,7 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
-pthread_mutex_t lock;            // declare a lock
+pthread_mutex_t locks[NBUCKET];            // declare a lock
 
 
 double
@@ -54,9 +54,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
-    pthread_mutex_lock(&lock);       // acquire lock
+    pthread_mutex_lock(&locks[i]);       // acquire lock
     insert(key, value, &table[i], table[i]);
-    pthread_mutex_unlock(&lock);     // release lock
+    pthread_mutex_unlock(&locks[i]);     // release lock
   }
 
 }
@@ -108,8 +108,9 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-  pthread_mutex_init(&lock, NULL); // initialize the lock
-  
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL); // initialize the lock
+  }
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
